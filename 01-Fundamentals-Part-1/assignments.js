17b3a0b0d2774cc32f10c7149ba2d1f4b052d448
ajfm88 <ale@ajfm88.com>
2021-12-14 10:36:22 -0800

    Assignment Lecture 10

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,10 @@
+// LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+let country = 'United States';
+let continent = 'America';
+let population = 330;
+console.log(country);
+console.log(continent);
+console.log(population);
