bcd0b1836a84cc759cce14655227789df275cd1c
ajfm88 <ale@ajfm88.com>
2021-12-30 10:54:37 -0800

    48. Looping Backwards and Loops in Loops

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -485,6 +485,33 @@ for (let i = 0; i < jonas.length; i++) {
 console.log('--- BREAK WITH NUMBER ---')
 for (let i = 0; i < jonas.length; i++) {
   if (typeof jonas[i] === 'number') break;
-  
+
   console.log(jonas[i], typeof jonas[i]);
+}
+
+
+///////////////////////////////////////
+// Looping Backwards and Loops in Loops
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true
+];
+
+// 0, 1, ..., 4
+// 4, 3, ..., 0
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-------- Starting exercise ${exercise}`);
+  
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
+  }
 }
