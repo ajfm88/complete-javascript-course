442618f98ff0aa9a41a5df32a834e2eeb8a65694
ajfm88 <ale@ajfm88.com>
2022-01-22 15:02:59 -0800

    100. Primitives vs. Objects in Practice

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -3,7 +3,7 @@
 ///////////////////////////////////////
 // Scoping in Practice
 
-
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -55,12 +55,12 @@ let job = 'teacher';
 const year = 1991;
 
 // Functions
-console.log(addDecl(2,3));
-// console.log(addExpr(2,3));
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
 console.log(addArrow);
-// console.log(addArrow(2,3));
+// console.log(addArrow(2, 3));
 
-function addDecl(a,b) {
+function addDecl(a, b) {
   return a + b;
 }
 
@@ -71,8 +71,8 @@ const addExpr = function (a, b) {
 var addArrow = (a, b) => a + b;
 
 // Example
-console.log(numProducts);
-if(!numProducts) deleteShoppingCart();
+console.log(undefined);
+if (!numProducts) deleteShoppingCart();
 
 var numProducts = 10;
 
@@ -190,3 +190,43 @@ const friend = me;
 friend.age = 27;
 console.log('Friend:', friend);
 console.log('Me', me);
+
+
+///////////////////////////////////////
+// Primitives vs. Objects in Practice
+
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// Reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage: ', marriedJessica);
+// marriedJessica = {};
+
+// Copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage:', jessica2);
+console.log('After marriage: ', jessicaCopy);
+*/
