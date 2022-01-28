2efad83889fc1d1d9bbf4637b588851ef286751e
ajfm88 <ale@ajfm88.com>
2022-01-28 11:12:47 -0800

    108. The Nullish Coalescing Operator (??)

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -50,6 +50,16 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// The Nullish Coalescing Operator
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+///////////////////////////////////////
 // Short Circuiting (&& and ||)
 
 console.log('---- OR ----');
