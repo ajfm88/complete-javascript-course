2711fb211aa262247440715a72bd8755b3bcd68a
ajfm88 <ale@ajfm88.com>
2022-02-08 14:12:12 -0800

    125. String Methods Practice

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
   [weekdays[3]]: {
@@ -53,6 +49,28 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// String Methods Practice
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+  console.log(output);
+}
+
+///////////////////////////////////////
 // Coding Challenge #4
 
 /* 
@@ -94,7 +112,7 @@ document.querySelector('button').addEventListener('click', function () {
 
   for (const [i, row] of rows.entries()) {
     const [first, second] = row.toLowerCase().trim().split('_');
-    
+
     const output = `${first}${second.replace(
       second[0],
       second[0].toUpperCase()
@@ -106,7 +124,7 @@ document.querySelector('button').addEventListener('click', function () {
 
 /*
 ///////////////////////////////////////
-// Working With Strings - Part 3
+// Working With Strings - Part 2
 
 // Split and join
 console.log('a+very+nice+string'.split('+'));
@@ -598,7 +616,7 @@ for (const [i, el] of menu.entries()) {
 ///////////////////////////////////////
 // Coding Challenge #1
 
-/*
+/* 
 We're building a football betting app (soccer for my American friends 😅)!
 
 Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
