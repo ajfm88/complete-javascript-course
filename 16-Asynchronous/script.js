f1421bd0de5a88bbc3b9a3ef955f6218bd144177
ajfm88 <ale@ajfm88.com>
2022-03-29 20:10:14 -0700

    252. Consuming Promises

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -140,9 +140,24 @@ setTimeout(() => {
 }, 1000);
 */
 
-// const request = new XMLHttpRequest();
-// request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-// request.send();
+///////////////////////////////////////
+// Consuming Promises
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
 
-const request = fetch('https://restcountries.com/v3.1/name/portugal');
-console.log(request);
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => renderCountry(data[0]));
+};
+
+getCountryData('portugal');
