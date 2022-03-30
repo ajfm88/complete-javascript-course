f53cba10c98143f16c06771edac162a503fac883
ajfm88 <ale@ajfm88.com>
2022-03-29 20:26:05 -0700

    253. Chaining Promises

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -142,6 +142,8 @@ setTimeout(() => {
 
 ///////////////////////////////////////
 // Consuming Promises
+// Chaining Promises
+
 // const getCountryData = function (country) {
 //   fetch(`https://restcountries.com/v3.1/name/${country}`)
 //     .then(function (response) {
@@ -155,9 +157,20 @@ setTimeout(() => {
 // };
 
 const getCountryData = function (country) {
+  // Country 1
   fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then((response) => response.json())
-    .then((data) => renderCountry(data[0]));
+    .then(response => return response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
 getCountryData('portugal');
