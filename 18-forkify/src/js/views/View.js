23fe18ed73169190945ba41f27a2d9ece3a61351
ajfm88 <ale@ajfm88.com>
2022-04-16 16:32:28 -0700

    297. Implementing Search Results - Part 2

---
+++ 18-forkify/src/js/views/View.js
@@ -0,0 +1,34 @@
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+export default class View {
+  _data;
+
+  render(data, render = true) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
+
+    this._data = data;
+    const markup = this._generateMarkup();
+
+    if (!render) return markup;
+
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  _clear() {
+    this._parentElement.innerHTML = '';
+  }
+
+  renderSpinner() {
+    const markup = `
+      <div class="spinner">
+        <svg>
+          <use href="${icons}#icon-loader"></use>
+        </svg>
+      </div>
+    `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+}
